55c35a97db47bcf3ae6fda2b44fe8bd95d6ea021
vitoraugreis <vitoraugreis@gmail.com>
2025-11-27 16:56:34 -0300

    ToDo 5 concluído

--- src/Conta.java
+++ src/Conta.java
@@ -47,30 +47,32 @@ public class Conta {
             throw new IllegalArgumentException("Tipo de operação inválido: " + tipo);
     }
 
-    // Separa responsabilidade: gerar representação dos dados da conta
     private String gerarDadosConta() {
         return String.format("Ag.: %d\nConta: %d\nGerente: %s\nSaldo: %.2f",
                 this.numAgencia, this.numConta, this.gerente, this.saldo);
     }
-    
-    public String toString() {
-        String dadosCliente = this.cliente.toString();
-        String dadosConta = gerarDadosConta();
 
-        // TODO(#5) REFATORAR: Essa operação não deveria estar sendo realizada neste método
-        String dadosExtrato = "";
-        for(Operacao op : this.operacoes) {
-            dadosExtrato += op.toString() + "\n";
+    private String gerarDadosExtrato() {
+        String sb = "";
+        for (Operacao op : this.operacoes) {
+            sb += op.toString() + "\n";
         }
-
-        return "-----CLIENTE-----\n" +
-                dadosCliente +
-                "\n\n" +
-                "-----CONTA-----\n" +
-                dadosConta +
-                "\n\n" +
-                "-----EXTRATO-----\n" +
-                dadosExtrato +
-                "\n";
+        return sb.toString();
     }
-}
+     
+     public String toString() {
+         String dadosCliente = this.cliente.toString();
+         String dadosConta = gerarDadosConta();
+         String dadosExtrato = gerarDadosExtrato();
+
+         return "-----CLIENTE-----\n" +
+                 dadosCliente +
+                 "\n\n" +
+                 "-----CONTA-----\n" +
+                 dadosConta +
+                 "\n\n" +
+                 "-----EXTRATO-----\n" +
+                 dadosExtrato +
+                 "\n";
+     }
+ }
